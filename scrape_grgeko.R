1f448a4847fe01529cdbebb96a30aa7ddf280268
FLorenz <felix.lorenz@tg.ch>
2024-05-30 12:16:25 +0200

    three long instead of one wide csv

--- scrape_grgeko.R
+++ scrape_grgeko.R
@@ -193,8 +193,8 @@ scrape_grgeko <- function(legislatur = 2020) {
 prepare_ogd_vorstoesse <- function(data_list){
   # Vorstoesser
   vorstoesse <- data_list[[1]] %>% 
-    mutate(datum_vorstoss_eingang = lubridate::dmy(eintrittsdatum)) %>% 
-    select(datum_vorstoss_eingang,registraturnummer,grg_nummer,geschaftstitel,geschaftsart,kennung,sachbegriff,status,departement) 
+    mutate(datum_geschaeft_eingang = lubridate::dmy(eintrittsdatum)) %>% 
+    select(datum_geschaeft_eingang,registraturnummer,grg_nummer,geschaftstitel,geschaftsart,kennung,sachbegriff,status,departement) 
   
   max_splits <- max(sapply(strsplit(vorstoesse$sachbegriff, ", \\d"), length))
   
@@ -204,68 +204,70 @@ prepare_ogd_vorstoesse <- function(data_list){
   
   # Vorstoesse
   vorstoesser <- data_list[[2]] %>% 
-    group_by(titel,registraturnummer) %>% 
-    mutate(person_id = row_number()) %>%
-    ungroup() %>% 
-    tidyr::pivot_wider(
-      names_from = person_id,
-      values_from = c(nachname, vorname, ort, partei),
-      names_sep = "_"
-    ) 
+    filter(!if_all(c(nachname, vorname, partei), is.na))    
+
   
-  
-  column_names_vs <- colnames(vorstoesser)
-  
-  max_digit_vs <- column_names_vs %>% str_extract("\\d+") %>% as.numeric() %>% max(na.rm = T)
-  
-  new_order_vs <- c("nachname","vorname","partei","ort") %>% 
-    expand.grid(.,1:max_digit_vs) %>% 
-    mutate(name = paste0(Var1,"_",Var2)) %>% 
-    pull(name) %>% 
-    c("titel", "registraturnummer",.)
-  
-  vorstoesser_wide <- vorstoesser %>% 
-    select(all_of(new_order_vs))
-  
+  # 
+  # column_names_vs <- colnames(vorstoesser)
+  # 
+  # max_digit_vs <- column_names_vs %>% str_extract("\\d+") %>% as.numeric() %>% max(na.rm = T)
+  # 
+  # new_order_vs <- c("nachname","vorname","partei","ort") %>% 
+  #   expand.grid(.,1:max_digit_vs) %>% 
+  #   mutate(name = paste0(Var1,"_",Var2)) %>% 
+  #   pull(name) %>% 
+  #   c("titel", "registraturnummer",.)
+  # 
+  # vorstoesser_wide <- vorstoesser %>% 
+  #   select(all_of(new_order_vs))
+  # 
   
   # Dokumente
   
-  dokumente <- data_list[[3]] %>% 
-    group_by(titel,registraturnummer) %>% 
-    mutate(person_id = row_number()) %>%
-    ungroup() %>% 
-    tidyr::pivot_wider(
-      names_from = person_id,
-      values_from = c(doc_title,doc_link),
-      names_sep = "_"
-    ) 
+  dokumente <- data_list[[3]] %>%
+    filter(!is.na(doc_link))
+    # group_by(titel,registraturnummer) %>% 
+    # mutate(person_id = row_number()) %>%
+    # ungroup() %>% 
+    # tidyr::pivot_wider(
+    #   names_from = person_id,
+    #   values_from = c(doc_title,doc_link),
+    #   names_sep = "_"
+    # ) 
+    # 
+  
+  # column_names_doc <- colnames(vorstoesser)
+  
+  # max_digit_doc <- column_names_doc %>% str_extract("\\d+") %>% as.numeric() %>% max(na.rm = T)
+  # 
+  # new_order_doc <- c("doc_title","doc_link") %>% 
+  #   expand.grid(.,1:max_digit_doc) %>% 
+  #   mutate(name = paste0(Var1,"_",Var2)) %>% 
+  #   pull(name) %>% 
+  #   c("titel", "registraturnummer",.)
+  # 
+  # dokumente_wide <- dokumente %>% 
+  #   select(all_of(new_order_doc))
+  # 
+  # 
+  # full_vs_data <- vorstoesse_wide %>% 
+  #   left_join(vorstoesser_wide, by = c("geschaftstitel"="titel","registraturnummer")) %>% 
+  #   left_join(dokumente_wide, by = c("geschaftstitel"="titel","registraturnummer")) %>% 
+  #   rename(geschaeftsnummer = "registraturnummer",
+  #          grg_nr = "grg_nummer",
+  #          vorstoss_bezeichnung = "geschaftstitel",
+  #          vorstossart_bezeichnung = "geschaftsart",
+  #          vorstossart_kennung = "kennung")
   
   
-  column_names_doc <- colnames(vorstoesser)
+  write.table(vorstoesser, file = "vorstoesser.csv", quote = T, sep = ",", dec = ".", 
+              row.names = F, na="",fileEncoding = "utf-8")
+  write.table(dokumente, file = "dokumente.csv", quote = T, sep = ",", dec = ".", 
+              row.names = F, na="",fileEncoding = "utf-8")
+  write.table(vorstoesse_wide, file = "geschaefte.csv", quote = T, sep = ",", dec = ".", 
+              row.names = F, na="",fileEncoding = "utf-8")
   
-  max_digit_doc <- column_names_doc %>% str_extract("\\d+") %>% as.numeric() %>% max(na.rm = T)
-  
-  new_order_doc <- c("doc_title","doc_link") %>% 
-    expand.grid(.,1:max_digit_doc) %>% 
-    mutate(name = paste0(Var1,"_",Var2)) %>% 
-    pull(name) %>% 
-    c("titel", "registraturnummer",.)
-  
-  dokumente_wide <- dokumente %>% 
-    select(all_of(new_order_doc))
-  
-  
-  full_vs_data <- vorstoesse_wide %>% 
-    left_join(vorstoesser_wide, by = c("geschaftstitel"="titel","registraturnummer")) %>% 
-    left_join(dokumente_wide, by = c("geschaftstitel"="titel","registraturnummer")) %>% 
-    rename(geschaeftsnummer = "registraturnummer",
-           grg_nr = "grg_nummer",
-           vorstoss_bezeichnung = "geschaftstitel",
-           vorstossart_bezeichnung = "geschaftsart",
-           vorstossart_kennung = "kennung")
-  
-  return(full_vs_data)
-  
+
 }
 
 
--- user_scraper.R
+++ user_scraper.R
@@ -4,11 +4,7 @@ source("scrape_grgeko.R")
 geschafte_list <- scrape_grgeko(legislatur = 2024)
 
 # Daten aufbereiten für OGD 
-data_ogd <- prepare_ogd_vorstoesse(geschafte_list)
-
-
-write.table(data_ogd, file = "vorstoesse.csv", quote = T, sep = ",", dec = ".", 
-            row.names = F, na="",fileEncoding = "utf-8")
+prepare_ogd_vorstoesse(geschafte_list)
 
 
 saveRDS(Sys.time(),"last_run.rds")
